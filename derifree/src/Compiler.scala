5f774e6b093b0cc5d41f85c8d967d75490e34190
Christoph Bunte <christophbunte@gmail.com>
2023-12-02 07:36:55 +0100

    wip

--- derifree/src/Compiler.scala
+++ derifree/src/Compiler.scala
@@ -18,10 +18,7 @@ object Compiler:
     given monoid[T]: Monoid[Log[T]] = new Monoid[Log[T]]:
       def empty: Log[T] = Log(Map.empty, Set.empty)
       def combine(x: Log[T], y: Log[T]): Log[T] =
-        Log(
-          x.spotObservations <+> y.spotObservations,
-          x.payments <+> y.payments
-        )
+        Log(x.spotObservations <+> y.spotObservations, x.payments <+> y.payments)
 
   /** Relization of a piecewise diffusion. */
   case class Simulation[T](
@@ -58,20 +55,12 @@ object Compiler:
         case dsl.Cashflow(amount, time) => Writer(Log.payment(time), PV(1.0))
 
         case dsl.HitProb(dsl.Barrier.Discrete(_, _, levels)) =>
-          Writer(
-            levels.toList.foldMap((ticker, obs) =>
-              Log.spotObs(ticker, obs.map(_(0)).toSet)
-            ),
-            0.5
-          )
+          Writer(levels.toList.foldMap((ticker, obs) => Log.spotObs(ticker, obs.map(_(0)).toSet)), 0.5)
 
         case dsl.HitProb(dsl.Barrier.Continuous(_, _, levels, from, to)) =>
           Writer(
             levels.keys.toList.foldMap(ticker =>
-              Log.spotObs(
-                ticker,
-                Set(from, to) <+> TimeOrder[T].dailyStepsBetween(from, to).toSet
-              )
+              Log.spotObs(ticker, Set(from, to) <+> TimeOrder[T].dailyStepsBetween(from, to).toSet)
             ),
             0.5
           )
@@ -95,48 +84,69 @@ object Compiler:
             s"missing time index for $time"
           )
 
-        case dsl.HitProb(dsl.Barrier.Discrete(direction, policy, levels)) => ???
+        case dsl.HitProb(dsl.Barrier.Discrete(direction, policy, levels)) =>
+          (
+            levels.keys.toList
+              .traverse(ticker =>
+                Either.fromOption(sim.spots.get(ticker), s"missing spots for $ticker").tupleLeft(ticker)
+              )
+              .map(_.toMap),
+            levels.keys.toList
+              .traverse(ticker =>
+                Either.fromOption(sim.jumps.get(ticker), s"missing jumps for $ticker").tupleLeft(ticker)
+              )
+              .map(_.toMap),
+            levels.keys.toList
+              .traverse(ticker =>
+                Either.fromOption(sim.vols.get(ticker), s"missing vols for $ticker").tupleLeft(ticker)
+              )
+              .map(_.toMap),
+          ).mapN { case (spots, jumps, vols) =>
+            val sign = direction match {
+              case dsl.Barrier.Direction.Up   => 1
+              case dsl.Barrier.Direction.Down => -1
+            }
 
-        case dsl.HitProb(
-              dsl.Barrier.Continuous(direction, policy, levels, from, to)
-            ) =>
+            policy match
+              case dsl.Barrier.Policy.And =>
+                val hitTimes = levels
+                  .map((ticker, obs) =>
+                    ticker -> obs
+                      .filter((time, level) => sign * (spots(ticker)(sim.timeIndex(time)) - level) >= 0)
+                      .map(_(0))
+                      .toSet
+                  )
+                  .values
+                  .reduce(_ intersect _)
+                if hitTimes.nonEmpty then 1.0 else 0.0
+              case dsl.Barrier.Policy.Or =>
+                val hit = levels.exists((ticker, obs) =>
+                  obs.exists((time, level) => sign * (spots(ticker)(sim.timeIndex(time)) - level) >= 0)
+                )
+                if hit then 1.0 else 0.0
+
+          }
+
+        case dsl.HitProb(dsl.Barrier.Continuous(direction, policy, levels, from, to)) =>
           Either.raiseUnless(policy == dsl.Barrier.Policy.Or)(
             "Only `or` barriers are supported for continuous monitoring"
           ) *>
             (
-              Either.fromOption(
-                sim.timeIndex.get(from),
-                "missing `from` index"
-              ),
+              Either.fromOption(sim.timeIndex.get(from), "missing `from` index"),
               Either.fromOption(sim.timeIndex.get(to), "missing `to` index"),
               levels.keys.toList
                 .traverse(ticker =>
-                  Either
-                    .fromOption(
-                      sim.spots.get(ticker),
-                      s"missing spots for $ticker"
-                    )
-                    .tupleLeft(ticker)
+                  Either.fromOption(sim.spots.get(ticker), s"missing spots for $ticker").tupleLeft(ticker)
                 )
                 .map(_.toMap),
               levels.keys.toList
                 .traverse(ticker =>
-                  Either
-                    .fromOption(
-                      sim.jumps.get(ticker),
-                      s"missing jumps for $ticker"
-                    )
-                    .tupleLeft(ticker)
+                  Either.fromOption(sim.jumps.get(ticker), s"missing jumps for $ticker").tupleLeft(ticker)
                 )
                 .map(_.toMap),
               levels.keys.toList
                 .traverse(ticker =>
-                  Either
-                    .fromOption(
-                      sim.vols.get(ticker),
-                      s"missing vols for $ticker"
-                    )
-                    .tupleLeft(ticker)
+                  Either.fromOption(sim.vols.get(ticker), s"missing vols for $ticker").tupleLeft(ticker)
                 )
                 .map(_.toMap),
             ).mapN { case (i1, i2, spots, jumps, vols) =>
@@ -153,16 +163,14 @@ object Compiler:
                 while (i < i2) {
                   val dt = sim.deltaTs(i)
                   val s0 = s(i)
-                  // we want s1 = S(t_{i+1}-) (limit from the left)
-                  // and use Jump(t_i) = S(t_i) - S(t_i-)
+                  // we want s1 = S(t_{i+1}-) (limit from the left) and use Jump(t_i) = S(t_i) - S(t_i-)
                   val s1 = s(i + 1) - jmp(i + 1)
+                  val s1r = s(i + 1)
                   val vol = v(i)
-                  if (sign * (s0 - level) <= 0 || sign * (s1 - level) <= 0) {
+                  if (sign * (s0 - level) <= 0 || sign * (s1 - level) <= 0 || sign * (s1r - level) <= 0) {
                     p = 0.0
                   } else {
-                    val u = math.log(level / s0) * math.log(
-                      level / s1
-                    ) / (dt * vol * vol)
+                    val u = math.log(level / s0) * math.log(level / s1) / (dt * vol * vol)
                     p *= math.exp(sign * 2.0 * u)
                   }
                   i += 1
