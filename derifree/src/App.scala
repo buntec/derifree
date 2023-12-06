7a9e0709583cb79f531a61180f4f047d7723b276
Christoph Bunte <christophbunte@gmail.com>
2023-12-06 14:33:15 +0100

    wip

--- derifree/src/App.scala
+++ derifree/src/App.scala
@@ -6,7 +6,8 @@ import derifree.Sobol
 
 object Main extends IOApp.Simple:
 
-  def run: IO[Unit] = Sobol[IO](100).flatMap { sobol =>
+  def run: IO[Unit] = Sobol.directionNumbersFromResource[IO](100).flatMap { dirNums =>
+    val sobol = Sobol(10, dirNums)
     val seq = sobol.next.replicateA(10).runA(sobol.initialState).value
     IO.println(seq)
   }
--- derifree/src/Sobol.scala
+++ derifree/src/Sobol.scala
@@ -1,12 +1,54 @@
+/*-----------------------------------------------------------------------------
+ * Licence pertaining to construction of Sobol numbers and the accompanying sets of direction numbers
+ *
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2008, Frances Y. Kuo and Stephen Joe
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the names of the copyright holders nor the names of the
+ *       University of New South Wales and the University of Waikato
+ *       and its contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package derifree
 
 import cats.data.{State => cState}
 import cats.syntax.all.*
 import cats.MonadThrow
-import cats.effect.Sync
+import cats.effect.*
+import fs2.Stream
 
 import Sobol.*
+import fs2.io.file.Files
+import fs2.io.file.Path
 
+import scala.collection.immutable.ArraySeq
+
+/*
+ * https://web.maths.unsw.edu.au/~fkuo/sobol/
+ */
 trait Sobol:
 
   def initialState: State
@@ -17,17 +59,30 @@ trait Sobol:
 
 object Sobol:
 
+  opaque type DirectionNumbers = IndexedSeq[IndexedSeq[Int]]
+
+  object DirectionNumbers:
+
+    private[Sobol] def apply(dirs: IndexedSeq[IndexedSeq[Int]]): DirectionNumbers = dirs
+
+    extension (dirs: DirectionNumbers)
+      def get: IndexedSeq[IndexedSeq[Int]] = dirs
+      def maxDim: Int = dirs.length
+
   case class State(pos: Int, x: IndexedSeq[Int])
 
-  def apply[F[_]: Sync](dim: Int): F[Sobol] =
-    directionNumbersFromResource[F](dim).map(apply(dim, _))
+  def directionNumbersFromResource[F[_]: Concurrent: Sync](maxDim: Int): F[DirectionNumbers] =
+    directionNumbersFromByteStream[F](fs2.io.readClassLoaderResource[F]("new-joe-kuo-6.21201"), maxDim)
 
-  def apply(dim: Int, directionNumbers: IndexedSeq[IndexedSeq[Int]]): Sobol = new Sobol:
+  def directionNumbersFromFile[F[_]: Concurrent: Files](path: Path, maxDim: Int): F[DirectionNumbers] =
+    directionNumbersFromByteStream[F](Files[F].readAll(path), maxDim)
+
+  def apply(dim: Int, directionNumbers: DirectionNumbers): Sobol = new Sobol:
 
     // skip first dimension: (0,...,0)
-    val initialState: State = next.runS(State(0, Array.ofDim[Int](dim).toIndexedSeq)).value
+    def initialState: State = next.runS(State(0, Array.ofDim[Int](dim).toIndexedSeq)).value
 
-    val next: cState[State, IndexedSeq[Double]] =
+    def next: cState[State, IndexedSeq[Double]] =
       cState[State, IndexedSeq[Double]]: state =>
         val out = Array.ofDim[Double](dim)
         if (state.pos == 0) then
@@ -36,7 +91,7 @@ object Sobol:
             out(i) = 0.0
             i += 1
           }
-          (state.copy(pos = state.pos + 1), IArray.unsafeFromArray(out))
+          (state.copy(pos = state.pos + 1), ArraySeq.unsafeWrapArray(out))
         else
           var c = 1
           var value = state.pos - 1
@@ -52,7 +107,7 @@ object Sobol:
             out(i) = x(i) / SCALE
             i += 1
           }
-          (state.copy(pos = state.pos + 1, IArray.unsafeFromArray(x)), IArray.unsafeFromArray(out))
+          (state.copy(pos = state.pos + 1, ArraySeq.unsafeWrapArray(x)), ArraySeq.unsafeWrapArray(out))
 
     def skipTo(pos: Int): cState[State, Unit] =
       require(
@@ -84,14 +139,16 @@ object Sobol:
           j += 1
         }
       }
-      cState.set(State(pos, IArray.unsafeFromArray(x)))
+      cState.set(State(pos, ArraySeq.unsafeWrapArray(x)))
 
-  private val BITS = 31
-  private val MAXDIM = 21201
-  private val MAXPOS = math.pow(2, BITS).toInt - 1
-  private val SCALE = math.pow(2, BITS)
+  private val BITS: Int = 31
+  private val MAXPOS: Int = ((1L << BITS) - 1).toInt
+  private val SCALE: Double = (1L << BITS).toDouble
 
-  private def directionNumbersFromResource[F[_]: Sync](dim: Int): F[IndexedSeq[IndexedSeq[Int]]] =
+  private def directionNumbersFromByteStream[F[_]: Concurrent](
+      bytes: Stream[F, Byte],
+      maxDim: Int
+  ): F[DirectionNumbers] =
 
     def parseLine(line: String): Option[IndexedSeq[Int]] =
       val tokens = line.split(" ").toList.filter(_.nonEmpty).traverse(_.toIntOption)
@@ -125,14 +182,15 @@ object Sobol:
       }
       dirs.toIndexedSeq
 
-    val lines = fs2.io
-      .readClassLoaderResource[F]("new-joe-kuo-6.21201")
-      .through(fs2.text.utf8.decode)
-      .through(fs2.text.lines)
-      .drop(1) // drop header
-      .take(dim)
+    val lines =
+      bytes
+        .through(fs2.text.utf8.decode)
+        .through(fs2.text.lines)
+        .drop(1) // drop header
+        .take(maxDim)
 
-    MonadThrow[F].raiseWhen(dim > MAXDIM)(new IllegalArgumentException(s"dim must not exceed $MAXDIM but was $dim")) *>
-      (fs2.Stream(d0).covary[F] ++ lines.evalMap: line =>
-        MonadThrow[F].fromOption(parseLine(line), new Exception(s"failed to parse line: $line"))).compile.toList
-        .map(_.toArray.toIndexedSeq)
+    (fs2.Stream(d0).covary[F] ++ lines.evalMap: line =>
+      MonadThrow[F].fromOption(parseLine(line), new Exception(s"failed to parse line: $line"))).compile.toList
+      .map(_.toArray.toIndexedSeq)
+      .map(DirectionNumbers(_))
+      .ensure(new IllegalArgumentException("found fewer than the requested number of dimensions"))(_.length >= maxDim)
