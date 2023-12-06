0119b045cffd675887468deaa1c1bddda978dce8
Christoph Bunte <christophbunte@gmail.com>
2023-12-06 17:36:16 +0100

    wip

---
+++ derifree/src/BrownianBridge.scala
@@ -0,0 +1,83 @@
+package derifree
+
+import org.apache.commons.math3.util.FastMath.sqrt
+
+object BrownianBridge:
+
+  /** Transforms an array of standard normals into another array of standard normals. For
+    * performance reasons this is done in-place and without allocations.
+    */
+  def transform(numberOfSteps: Int): Array[Double] => Unit =
+
+    // ===========================================================================
+    // NOTE: The following copyright notice applies to the original C++ code,
+    //
+    // Copyright (C) 2002 Peter Jäckel "Monte Carlo Methods in Finance".
+    // All rights reserved.
+    //
+    // Permission to use, copy, modify, and distribute this software is freely
+    // granted, provided that this notice is preserved.
+    // ===========================================================================
+
+    val leftIndex, rightIndex, bridgeIndex = Array.fill(numberOfSteps)(0)
+    val leftWeight, rightWeight, stddev = Array.fill(numberOfSteps)(0.0)
+    val map = Array.fill(numberOfSteps)(0)
+    val path = Array.fill(numberOfSteps)(0.0)
+
+    map(numberOfSteps - 1) = 1
+    stddev(0) = sqrt(numberOfSteps.toDouble)
+    bridgeIndex(0) = numberOfSteps - 1
+    var j = 0
+    var i = 1
+    while (i < numberOfSteps) {
+      while (map(j) != 0) {
+        j += 1
+      }
+      var k = j
+      while (map(k) == 0) {
+        k += 1
+      }
+      val l = j + ((k - 1 - j) >>> 1)
+      map(l) = i
+      bridgeIndex(i) = l
+      leftIndex(i) = j
+      rightIndex(i) = k
+      leftWeight(i) = (k - l) / (k + 1.0 - j)
+      rightWeight(i) = (l + 1.0 - j) / (k + 1.0 - j)
+      stddev(i) = sqrt((l + 1.0 - j) * (k - l) / (k + 1.0 - j))
+      j = k + 1
+      if (j >= numberOfSteps) {
+        j = 0
+      }
+      i += 1
+    }
+
+    def buildPath(standardNormals: Array[Double]): Array[Double] = {
+      path(numberOfSteps - 1) = stddev(0) * standardNormals(0)
+      var i = 1
+      while (i < numberOfSteps) {
+        val j = leftIndex(i)
+        val k = rightIndex(i)
+        val l = bridgeIndex(i)
+        if (j != 0) {
+          path(l) = leftWeight(i) * path(j - 1) + rightWeight(i) * path(
+            k
+          ) + stddev(i) * standardNormals(i)
+        } else {
+          path(l) = rightWeight(i) * path(k) + stddev(i) * standardNormals(i)
+        }
+        i += 1
+      }
+      path
+    }
+
+    (standardNormals: Array[Double]) => {
+      require(standardNormals.length == numberOfSteps)
+      val path = buildPath(standardNormals)
+      standardNormals(0) = path(0)
+      var i = 1
+      while (i < path.length) {
+        standardNormals(i) = path(i) - path(i - 1)
+        i += 1
+      }
+    }
--- derifree/src/NormalGen.scala
+++ derifree/src/NormalGen.scala
@@ -18,23 +18,34 @@ trait NormalGen:
 object NormalGen:
 
   def fromSobol(m: Int, n: Int, dirs: Sobol.DirectionNumbers): Either[Sobol.Error, NormalGen] =
-    Sobol(m * n, dirs).map(sobol =>
+    Sobol(m * n, dirs).map: sobol =>
+      val bbt = BrownianBridge.transform(m * n)
       new NormalGen:
         type State = Sobol.State
+
         def dimensions: (Int, Int) = (m, n)
+
         def init: State = sobol.initialState
+
         def next: cState[State, IndexedSeq[IndexedSeq[Double]]] =
-          sobol.next.map { sobolNumbers =>
-            val zs = Array.ofDim[Double](m, n)
+          sobol.next.map: sobolNumbers =>
+            val zs0 = sobolNumbers.toArray
             var i = 0
+            while (i < m * n) {
+              zs0(i) = normal.inverseCdf(zs0(i))
+              i += 1
+            }
+            bbt(zs0)
+            val zs = Array.ofDim[Double](m, n)
+
+            i = 0
             while (i < m) {
               var j = 0
               while (j < n) {
-                zs(i)(j) = normal.inverseCdf(sobolNumbers(i * n + j))
+                zs(i)(j) = zs0(i * n + j)
                 j += 1
               }
               i += 1
             }
+
             ArraySeq.unsafeWrapArray(zs.map(ArraySeq.unsafeWrapArray))
-          }
-    )
